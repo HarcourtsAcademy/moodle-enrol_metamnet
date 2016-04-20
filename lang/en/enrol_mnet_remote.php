eb71fc64bc8e99e3534df5f68317647e80292918
Tim Butler <tim.butler@harcourts.net>
2016-04-20 10:17:46 +1000

    Added initial version.php, access.php and lang files.

---
+++ lang/en/enrol_mnet_remote.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Strings for component 'enrol_mnet_remote', language 'en'.
+ *
+ * @package     enrol_mnet_remote
+ * @author      Tim Butler
+ * @copyright   2016 Harcourts International Limited {@link http://www.harcourtsacademy.com}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Remote MNet Enrolment';
+$string['pluginname_desc'] = 'Remote MNet Enrolment plugin synchronises enrolments across Moodle MNet hosts.';

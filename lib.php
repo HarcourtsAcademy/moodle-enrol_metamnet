98af62baa22e52730e57b7c5008817bb2deb8bcd
Tim Butler <tim.butler@harcourts.net>
2016-04-20 21:11:14 +1000

    Added and refactored lib.php from enrol_meta plugin.

---
+++ lib.php
@@ -0,0 +1,291 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Meta MNet course enrolment plugin.
+ *
+ * @package     enrol_metamnet
+ * @author      Tim Butler
+ * @copyright   2016 Harcourts International Limited {@link http://www.harcourtsacademy.com}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class enrol_metamnet_plugin extends enrol_plugin {
+
+    /**
+     * Returns localised name of enrol instance
+     *
+     * @param stdClass $instance (null is accepted too)
+     * @return string
+     */
+    public function get_instance_name($instance) {
+        global $DB;
+
+        if (empty($instance)) {
+            $enrol = $this->get_name();
+            return get_string('pluginname', 'enrol_'.$enrol);
+        } else if (empty($instance->name)) {
+            /* todo: replace with code to include remote host and course.
+            $enrol = $this->get_name();
+            $course = $DB->get_record('course', array('id'=>$instance->customint1));
+            if ($course) {
+                $coursename = format_string(get_course_display_name_for_list($course));
+            } else {
+                // Use course id, if course is deleted.
+                $coursename = $instance->customint1;
+            }
+            return get_string('pluginname', 'enrol_' . $enrol) . ' (' . $coursename . ')';
+             */
+            return get_string('pluginname', 'enrol_'.$enrol); // todo: remote this line.
+        } else {
+            return format_string($instance->name);
+        }
+    }
+
+    /**
+     * Returns link to page which may be used to add new instance of enrolment plugin in course.
+     * @param int $courseid
+     * @return moodle_url page url
+     */
+    public function get_newinstance_link($courseid) {
+        $context = context_course::instance($courseid, MUST_EXIST);
+        if (!has_capability('moodle/course:enrolconfig', $context) or !has_capability('enrol/metamnet:config', $context)) {
+            return NULL;
+        }
+        // multiple instances supported - multiple remote courses linked
+        return new moodle_url('/enrol/metamnet/addinstance.php', array('id'=>$courseid));
+    }
+
+    /**
+     * Does this plugin allow manual unenrolment of a specific user? : No
+     *
+     * @param stdClass $instance course enrol instance
+     * @param stdClass $ue record from user_enrolments table
+     *
+     * @return bool - true means user with 'enrol/xxx:unenrol' may unenrol this user, false means nobody may touch this user enrolment
+     */
+    public function allow_unenrol_user(stdClass $instance, stdClass $ue) {
+        return false;
+    }
+
+    /**
+     * Gets an array of the user enrolment actions
+     *
+     * @param course_enrolment_manager $manager
+     * @param stdClass $ue A user enrolment object
+     * @return array An array of user_enrolment_actions
+     */
+    /* todo: check if we need this function at all or the defaults will do. 
+    public function get_user_enrolment_actions(course_enrolment_manager $manager, $ue) {
+        $actions = array();
+        $context = $manager->get_context();
+        $instance = $ue->enrolmentinstance;
+        $params = $manager->get_moodlepage()->url->params();
+        $params['ue'] = $ue->id;
+        if ($this->allow_unenrol_user($instance, $ue) && has_capability('enrol/metamnet:unenrol', $context)) {
+            $url = new moodle_url('/enrol/unenroluser.php', $params);
+            $actions[] = new user_enrolment_action(new pix_icon('t/delete', ''), get_string('unenrol', 'enrol'), $url, array('class'=>'unenrollink', 'rel'=>$ue->id));
+        }
+        return $actions;
+    }
+     */
+
+    /**
+     * Called after updating/inserting course.
+     *
+     * @param bool $inserted true if course just inserted
+     * @param stdClass $course
+     * @param stdClass $data form data
+     * @return void
+     */
+    /* todo: Check if we need to lib.php course_updated function.
+    public function course_updated($inserted, $course, $data) {
+        // Meta sync updates are slow, if enrolments get out of sync teacher will have to wait till next cron.
+        // We should probably add some sync button to the course enrol methods overview page.
+    }
+     * 
+     */
+
+    /**
+     * Update instance status
+     *
+     * @param stdClass $instance
+     * @param int $newstatus ENROL_INSTANCE_ENABLED, ENROL_INSTANCE_DISABLED
+     * @return void
+     */
+    public function update_status($instance, $newstatus) {
+        global $CFG;
+
+        parent::update_status($instance, $newstatus);
+
+        require_once("$CFG->dirroot/enrol/metamnet/locallib.php");
+        /* todo: manual trigger sync of a single course here?
+        enrol_meta_sync($instance->courseid);
+         * 
+         */
+    }
+
+    /**
+     * Called for all enabled enrol plugins that returned true from is_cron_required().
+     * @return void
+     */
+    public function cron() {
+        global $CFG;
+
+        require_once("$CFG->dirroot/enrol/metamnet/locallib.php");
+        
+        /* todo: manual trigger sync of ALL courses here?
+        enrol_meta_sync();
+         * 
+         */
+    }
+
+    /**
+     * Is it possible to delete enrol instance via standard UI?
+     *
+     * @param stdClass $instance
+     * @return bool
+     */
+    public function can_delete_instance($instance) {
+        $context = context_course::instance($instance->courseid);
+        return has_capability('enrol/metamnet:config', $context);
+    }
+
+    /**
+     * Is it possible to hide/show enrol instance via standard UI?
+     *
+     * @param stdClass $instance
+     * @return bool
+     */
+    public function can_hide_show_instance($instance) {
+        $context = context_course::instance($instance->courseid);
+        return has_capability('enrol/metamnet:config', $context);
+    }
+
+    /**
+     * Restore instance and map settings.
+     *
+     * @param restore_enrolments_structure_step $step
+     * @param stdClass $data
+     * @param stdClass $course
+     * @param int $oldid
+     */
+    /* todo: do we need the lib.php restore_instance function?
+    public function restore_instance(restore_enrolments_structure_step $step, stdClass $data, $course, $oldid) {
+        global $DB, $CFG;
+
+        if (!$step->get_task()->is_samesite()) {
+            // No meta restore from other sites.
+            $step->set_mapping('enrol', $oldid, 0);
+            return;
+        }
+
+        if (!empty($data->customint2)) {
+            $data->customint2 = $step->get_mappingid('group', $data->customint2);
+        }
+
+        if ($DB->record_exists('course', array('id' => $data->customint1))) {
+            $instance = $DB->get_record('enrol', array('roleid' => $data->roleid, 'customint1' => $data->customint1,
+                'courseid' => $course->id, 'enrol' => $this->get_name()));
+            if ($instance) {
+                $instanceid = $instance->id;
+            } else {
+                $instanceid = $this->add_instance($course, (array)$data);
+            }
+            $step->set_mapping('enrol', $oldid, $instanceid);
+
+            require_once("$CFG->dirroot/enrol/meta/locallib.php");
+            enrol_meta_sync($data->customint1);
+
+        } else {
+            $step->set_mapping('enrol', $oldid, 0);
+        }
+    }
+     * 
+     */
+
+    /**
+     * Restore user enrolment.
+     *
+     * @param restore_enrolments_structure_step $step
+     * @param stdClass $data
+     * @param stdClass $instance
+     * @param int $userid
+     * @param int $oldinstancestatus
+     */
+    /* todo: do we need the lib.php restore_user_enrolment function?
+    public function restore_user_enrolment(restore_enrolments_structure_step $step, $data, $instance, $userid, $oldinstancestatus) {
+        global $DB;
+
+        if ($this->get_config('unenrolaction') != ENROL_EXT_REMOVED_SUSPENDNOROLES) {
+            // Enrolments were already synchronised in restore_instance(), we do not want any suspended leftovers.
+            return;
+        }
+
+        // ENROL_EXT_REMOVED_SUSPENDNOROLES means all previous enrolments are restored
+        // but without roles and suspended.
+
+        if (!$DB->record_exists('user_enrolments', array('enrolid' => $instance->id, 'userid' => $userid))) {
+            $this->enrol_user($instance, $userid, null, $data->timestart, $data->timeend, ENROL_USER_SUSPENDED);
+            if ($instance->customint2) {
+                groups_add_member($instance->customint2, $userid, 'enrol_meta', $instance->id);
+            }
+        }
+    }
+     * 
+     */
+
+    /**
+     * Restore user group membership.
+     * @param stdClass $instance
+     * @param int $groupid
+     * @param int $userid
+     */
+    /* todo: Do we need the lib.php restore_group_member function?
+    public function restore_group_member($instance, $groupid, $userid) {
+        // Nothing to do here, the group members are added in $this->restore_group_restored().
+        return;
+    }
+     * 
+     */
+
+    /**
+     * Returns edit icons for the page with list of instances.
+     * @param stdClass $instance
+     * @return array
+     */
+    public function get_action_icons(stdClass $instance) {
+        global $OUTPUT;
+
+        if ($instance->enrol !== 'metamnet') {
+            throw new coding_exception('invalid enrol instance!');
+        }
+        $context = context_course::instance($instance->courseid);
+
+        $icons = array();
+
+        if (has_capability('enrol/metamnet:config', $context)) {
+            $editlink = new moodle_url("/enrol/metamnet/addinstance.php",
+                array('id' => $instance->courseid, 'enrolid' => $instance->id));
+            $icons[] = $OUTPUT->action_icon($editlink, new pix_icon('t/edit', get_string('edit'), 'core',
+                array('class' => 'iconsmall')));
+        }
+
+        return $icons;
+    }
+}
